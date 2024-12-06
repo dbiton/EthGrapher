db42c9505e6b7be730e70e76a5c5120a412e1c7a
dbiton <dbiton@gmail.com>
2024-12-06 11:58:38 +0200

    Make loaders and savers more efficient

--- loaders.py
+++ loaders.py
@@ -1,10 +1,30 @@
-from itertools import count
 import json
 import os
 import pickle
+import zlib
 
 import h5py
 
+from parsers import apply_recursively, bytes_to_hex
+
+
+def load_compressed_file(filepath: str, limit=None):
+    if os.path.exists(filepath):
+        with h5py.File(filepath, 'r') as f:
+            dset = f['dataset']
+            i = 0
+            for i_chunk in range(dset.shape[0]):
+                chunk = bytes(dset[i_chunk])
+                chunk = zlib.decompress(chunk)
+                chunk = pickle.loads(chunk)   
+                for value in chunk:
+                    i += 1
+                    print(f"loaded {i} values from {filepath}")
+                    yield apply_recursively(value, bytes_to_hex)
+                    if i == limit:
+                        return
+    else:
+        print("No traces file found.")
 
 def load_file(filepath: str, limit=None):
     if os.path.exists(filepath):
--- savers.py
+++ savers.py
@@ -1,8 +1,12 @@
-import json
+import math
 import os
+import pickle
 import h5py
+import zlib
+import numpy as np
 
 from fetchers import fetcher_prestate, fetch_parallel
+from parsers import apply_recursively, hex_to_bytes
 
 def save_prestate(filename: str, range_start: int, range_stop: int):
   generator = fetch_parallel(range_start, range_stop, fetcher_prestate)
@@ -13,21 +17,19 @@ def save_to_file(filename: str, generator, total_size) -> None:
   if os.path.exists(filename):
     raise Exception(f"{filename} already exists!")
   with h5py.File(filename, 'w') as f:
-      chunk_size = min(256, total_size)
+      chunk_size = min(128, total_size)
+      chunk_count = int(math.ceil(total_size/chunk_size))
       dset = f.create_dataset(
           'dataset',
-          shape=(total_size,),
-          dtype=h5py.string_dtype(encoding='ascii'),
-          chunks=(chunk_size,),
-          compression='gzip',
-          compression_opts=9
+          shape=(chunk_count,),
+          dtype=h5py.vlen_dtype(np.dtype('uint8')),
       )
-
       for i in range(0, total_size, chunk_size):
           start = i
           end = min(i + chunk_size, total_size)
-          chunk = []
-          for i in range(start, end):
-            chunk.append(json.dumps(next(generator)))
-          dset[start:end] = chunk
-          print(f"saved {i} values")
+          i_chunk = i // chunk_size
+          chunk = pickle.dumps([apply_recursively(next(generator), hex_to_bytes) for _ in range(start, end)])
+          chunk = zlib.compress(chunk, 3)
+          chunk = np.frombuffer(chunk, dtype=np.uint8)
+          dset[i_chunk] = chunk
+          print(f"Saved {end} values in total to {filename}")
